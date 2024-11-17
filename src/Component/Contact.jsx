df9cc9910cad1ed8e18cab0fd338e2da1c0d81c4
USER <mohamedmezoo210443@gmail.com>
2024-11-17 19:01:20 +0200

    last commit

--- src/Component/Contact.jsx
+++ src/Component/Contact.jsx
@@ -1,9 +1,15 @@
 /* eslint-disable no-unused-vars */
 import { motion } from 'framer-motion'
 import React from 'react'
+import cv from "../assets/projects/Gray and White Simple Clean Resume.pdf"
+
 
 
 function Contact() {
+  const download=(e)=>{
+    e.preventDefault()
+
+  }
    
   return (
     <div className='mt-10 text-center flex flex-col gap-6 mb-5'>
@@ -23,6 +29,11 @@ function Contact() {
         transition={{duration:1}}  
         >01124151053</motion.p>
         <motion.p>mohamedessam21044@gmail.com</motion.p>
+        <motion.button   whileInView={{opacity:1,x:0}}
+        initial={{opacity:0,x:100}}
+        transition={{duration:1}} className='bg-white text-black px-3 py-2 w-[fit-content] mx-auto rounded-2'>
+          <a onClick={()=>download()} href={cv}download={cv} >Download resume</a>
+        </motion.button>
     </div>
   )
 }
